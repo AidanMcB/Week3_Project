9922650cd76c94fc367f02f03e3089b9fb369a2c
AidanMcB <aidankmcbride@gmail.com>
2020-03-24 17:53:25 -0500

    adding to program.rb file

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ActiveRecord::Base
     has_many(:tickets)
     has_many(:viewers, through: :tickets)
+
+
+    def self.movie_titles
+        self.all.map {|movie| movie.title}
+    end
 end
