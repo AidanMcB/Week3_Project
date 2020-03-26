6c3a2f4e158edf0c109acca106fb08254aa928f6
melkilic <melike180400@gmail.com>
2020-03-26 17:27:33 -0500

    12

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,17 +11,12 @@ class Movie < ActiveRecord::Base
         self.all.map {|movie| movie.genre}
     end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     def self.ratings
         self.all.map{|movie| movie.rating}
     end
 
 
-=======
->>>>>>> Aidan
-=======
->>>>>>> 85d849c92c2277f5f1048d5c229a48573297b0f0
     def self.locations
         Ticket.all.map{|ticket| ticket.location}
     end
@@ -32,18 +27,10 @@ class Movie < ActiveRecord::Base
 
     end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 85d849c92c2277f5f1048d5c229a48573297b0f0
+
     def self.ratings(min, max)
         rating = self.all.select {|movie| movie.rating >= min && movie.rating <= max}
         rating.map {|movie| "#{movie.title}" + " " +  "#{movie.rating.to_s}" + " out of 10" }
     end
-<<<<<<< HEAD
->>>>>>> Aidan
-=======
 
->>>>>>> 85d849c92c2277f5f1048d5c229a48573297b0f0
 end
